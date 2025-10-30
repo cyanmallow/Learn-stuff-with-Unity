eaa8d6d1a620cf842352b1abb100a037642c02f7
Ginger <ggmmallow@gmail.com>
2025-10-30 15:21:27 +0700

    Add files via upload

---
+++ Junior Programmer. Player Control. Challenge/SpinScript.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SpinScript : MonoBehaviour
+{
+    public float rotationSpeed;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.forward * rotationSpeed * Time.deltaTime);
+
+    }
+}
